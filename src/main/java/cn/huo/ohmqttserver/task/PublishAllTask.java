e684764a5a2a737cde100b34023f3618640efbe9
huo65 <1599272181@qq.com>
2025-05-18 16:12:21 +0800

    改进消息发送格式

--- src/main/java/cn/huo/ohmqttserver/task/PublishAllTask.java
+++ src/main/java/cn/huo/ohmqttserver/task/PublishAllTask.java
@@ -2,11 +2,15 @@ package cn.huo.ohmqttserver.task;
 
 import cn.huo.ohmqttserver.service.AliveService;
 import org.dromara.mica.mqtt.core.server.MqttServer;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 /**
@@ -22,7 +26,7 @@ public class PublishAllTask {
 	/**
 	 * 测试连接
 	 */
-	@Scheduled(fixedDelay = 10000)
+	@Scheduled(fixedDelay = 15000)
 	public void run() {
 		System.out.println("PublishAllTask run");
 		mqttServer.publishAll("/test/123", "huo测试连接".getBytes(StandardCharsets.UTF_8));
@@ -36,12 +40,24 @@ public class PublishAllTask {
 		Set<String> aliveList = aliveService.getAliveList();
 		String message = null;
 		if (aliveList!= null && !aliveList.isEmpty()) {
-			message = "{\"aliveDevice\":[" + String.join(",", aliveList) + "]}";
+			message = createMessage(aliveList);
 		}
 		if (message != null) {
 			System.out.println("PublishAllTask alive");
 			mqttServer.publishAll("/device/list", message.getBytes(StandardCharsets.UTF_8));
 		}
+	}
+	public String createMessage(Set<String> aliveList) {
+		List<String> sortedList = new ArrayList<>(aliveList); // Set -> List
+		JSONArray jsonArray = new JSONArray();
+		for (String item : sortedList) {
+			jsonArray.put(item);
+		}
+
+		JSONObject jsonObject = new JSONObject();
+		jsonObject.put("aliveDevice", jsonArray);
+
+		return jsonObject.toString();
 	}
 
 }

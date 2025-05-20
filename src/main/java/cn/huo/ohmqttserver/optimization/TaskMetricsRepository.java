5533e9d5fc8f1aaca592741d2ab6346c6e4c1800
huo65 <1599272181@qq.com>
2025-05-20 19:55:08 +0800

    清理旧代码

--- src/main/java/cn/huo/ohmqttserver/optimization/TaskMetricsRepository.java
+++
@@ -1,7 +0,0 @@
-//package cn.huo.ohmqttserver.optimization;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface TaskMetricsRepository extends JpaRepository<TaskMetrics, Long> {
-//    // 可以根据需求添加查询方法
-//}
